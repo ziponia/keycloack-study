81c8d39aaa0ca9782670d05a32e0706d6ca5497e
ziponia <thtjwls@gmail.com>
2021-03-02 22:42:42 +0900

    Add Realm AuthServer example

--- src/main/java/com/example/demo/SecureController.java
+++ src/main/java/com/example/demo/SecureController.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 public class SecureController {
 
     @GetMapping("/customer")
-    public Principal secureHome(Principal principal) {
-        return principal;
+    public Object secureHome(Authentication authentication) {
+        System.out.println("principal: " + authentication.getName());
+        System.out.println(authentication.getAuthorities().toString());
+
+        // ex) 00a799f1-c8c5-4061-9928-2d1fdbabad8b - [ROLE_offline_access, ROLE_user, ROLE_uma_authorization]
+        return authentication.getName() + " - " + authentication.getAuthorities();
+
     }
 }
